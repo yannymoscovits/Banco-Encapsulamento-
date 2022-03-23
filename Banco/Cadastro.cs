d3b7b04a5838fe813fc7dc783fa8dfb2a459ada6
Yanny Dórea Moscovits <49878332+yannymoscovits@users.noreply.github.com>
2022-03-22 21:30:15 -0300

    Adicionar arquivos de projeto.

---
+++ Banco/Cadastro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Cadastro
+    {
+        public int NumerodaConta { get; private set; }
+        public string Nome { get; set; }
+        public double DepositoInicial { get; set; }
+
+        public Cadastro (int NumerodaConta, string Nome, double DepositoInicial)
+        {
+            this.NumerodaConta = NumerodaConta;
+            this.Nome = Nome;
+            this.DepositoInicial = DepositoInicial;
+        }
+
+        public double Deposito(double add)
+        {
+            this.DepositoInicial = this.DepositoInicial + add;
+            return this.DepositoInicial;
+        }
+
+        public double Saque(double sub)
+        {
+            this.DepositoInicial = this.DepositoInicial - sub - 5.00;
+            return this.DepositoInicial;
+        }
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double depositoinicial;
+
+            Console.WriteLine("Seja Bem-Vindo(a) ao Banco Moscovits! Para se cadastrar forneça os dados abaixo!" );
+
+            Console.WriteLine("Entre com o numero da conta : ");
+            int numeroconta = int.Parse( Console.ReadLine() );
+            Console.WriteLine("Entre o titular da conta: ");
+            string titularconta = Console.ReadLine();
+            Console.WriteLine("Havéra deposito inicial?");
+            string escolha = Console.ReadLine();
+
+            if (escolha == "s")
+            {
+                Console.Write("Entre o valor de depósito inicial : ");
+                depositoinicial = double.Parse( Console.ReadLine() );
+               
+            }
+            else
+            {
+                depositoinicial = 0.0;
+                
+            }
+            
+            Cadastro c1 = new Cadastro(numeroconta, titularconta, depositoinicial);
+
+            Console.WriteLine("Dados da Conta : ");
+            Console.WriteLine("Conta: " + numeroconta + ", " + " Titular: " + titularconta + ", " + " Saldo: " + depositoinicial.ToString("F2"));
+
+            Console.WriteLine("Entre com um valor de deposito :");
+            double add = double.Parse( Console.ReadLine() );
+            Console.WriteLine("Dados da Conta atualizados: ");
+            Console.WriteLine("Conta: " + numeroconta + ", " + " Titular: " + titularconta + ", " + " Saldo: " + c1.Deposito(add).ToString("F2"));
+
+            Console.WriteLine("Entre com um valor de saque :");
+            double sub = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dados da Conta atualizados : ");
+            Console.WriteLine("Conta: " + numeroconta + ", " + " Titular: " + titularconta + ", " + " Saldo: " + c1.Saque(sub).ToString("F2"));
+
+        }
+    }
+}
